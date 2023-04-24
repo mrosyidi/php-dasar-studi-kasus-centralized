72500d083879bfa72e76ab92a78a9429ba71e7f3
mrosyidi <rosyidiyuekunchen@gmail.com>
2023-04-25 06:22:59 +0700

    update ViewRemoveTodoList.php proses menghapus todolist

--- View/ViewRemoveTodoList.php
+++ View/ViewRemoveTodoList.php
@@ -1,5 +1,21 @@
 <?php
+  require_once __DIR__ . "/../BusinessLogic/RemoveTodoList.php";
+  require_once __DIR__ . "/../Helper/Input.php";
+
   function viewRemoveTodoList()
   {
+    echo "MENGHAPUS TODO" . PHP_EOL;
 
+    $pilihan = input("Nomor (x untuk batalkan)");
+
+    if($pilihan == "x"){
+      echo "Batal menghapus todo" . PHP_EOL;
+    }else{
+      $success = removeTodoList($pilihan);
+      if($success){
+        echo "Sukses menghapus todo nomor $pilihan" . PHP_EOL;
+      }else{
+        echo "Gagal menghapus todo nomor $pilihan" . PHP_EOL;
+      }
+    }
   }
